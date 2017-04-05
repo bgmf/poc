688065a3a1fa9ff69677fd4fce74b2d1998e9861
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2017-04-05 14:06:35 +0200

    fixed some issues with the plugin loader
    when loading multiple plugins, they got stuck within a single
    classloader, making it impossible to work with the plugins as intended.

--- shared/src/main/java/eu/dzim/shared/fx/integration/impl/PluginServiceLoader.java
+++ shared/src/main/java/eu/dzim/shared/fx/integration/impl/PluginServiceLoader.java
@@ -1,16 +1,19 @@
 package eu.dzim.shared.fx.integration.impl;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -25,7 +28,7 @@ public class PluginServiceLoader implements PluginIntegrator {
 	
 	private static final Logger LOG = LogManager.getLogger(PluginServiceLoader.class.getName());
 	
-	private static final String JAR_PREFIX = "plugin.jar";
+	private static final String JAR_FILE = "plugin.jar";
 	
 	private final ObservableMap<String, Plugin> plugins = FXCollections.observableMap(new TreeMap<>());
 	
@@ -38,42 +41,44 @@ public class PluginServiceLoader implements PluginIntegrator {
 		return plugins;
 	}
 	
-	private <T> Pair<ServiceLoader<T>, ClassLoader> loadIntegrations(Path path, Class<T> clazz) {
+	private <T> List<Pair<ServiceLoader<T>, ClassLoader>> loadIntegrations(Path path, Class<T> clazz) {
 		if (path != null) {
-			if (path.toFile().isFile() && path.getFileName().toString().endsWith(JAR_PREFIX)) {
-				List<URL> fileNames = new ArrayList<>();
-				try {
-					fileNames.add(path.toUri().toURL());
-				} catch (IOException e) {
-					LOG.error(e.getMessage(), e);
-				}
-				return loadJarIntegrations(fileNames, clazz);
+			if (path.toFile().isFile() && path.getFileName().toString().endsWith(JAR_FILE)) {
+				return Collections.singletonList(loadJarIntegrations(Collections.singletonList(path), clazz));
 			}
-			List<URL> jarFileUrls = findJarFiles(path);
-			if (jarFileUrls.isEmpty()) {
-				return loadDirecotryIntegrations(path, clazz);
+			List<Path> jarFiles = findJarFiles(path);
+			if (jarFiles.isEmpty()) {
+				return Collections.singletonList(loadDirecotryIntegrations(path, clazz));
 			} else {
-				return loadJarIntegrations(jarFileUrls, clazz);
+				return jarFiles.stream().map(jarFile -> loadDirecotryIntegrations(jarFile, clazz)).collect(Collectors.toList());
 			}
 		} else {
-			return new Pair<>(ServiceLoader.load(clazz), getClass().getClassLoader());
+			return Collections.singletonList(new Pair<>(ServiceLoader.load(clazz), getClass().getClassLoader()));
 		}
 	}
 	
-	private <T> Pair<ServiceLoader<T>, ClassLoader> loadJarIntegrations(List<URL> fileNames, Class<T> clazz) {
-		URL[] array = fileNames.stream().toArray(size -> new URL[size]);
+	private <T> Pair<ServiceLoader<T>, ClassLoader> loadJarIntegrations(List<Path> files, Class<T> clazz) {
+		// TODO add sub-directory of the same name and all its containing jars
+		URL[] array = files.stream().map(path -> {
+			try {
+				return path.toUri().toURL();
+			} catch (MalformedURLException e) {
+				LOG.error(e);
+				return null;
+			}
+		}).filter(url -> url != null).toArray(size -> new URL[size]);
 		URLClassLoader cl = new URLClassLoader(array, getClass().getClassLoader());
 		return new Pair<>(ServiceLoader.load(clazz, cl), cl);
 	}
 	
-	private List<URL> findJarFiles(Path path) {
-		List<URL> fileNames = new ArrayList<>();
+	private List<Path> findJarFiles(Path path) {
+		List<Path> fileNames = new ArrayList<>();
 		// FIXME use a glob instead of the filter below
 		try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
 			for (Path each : directoryStream) {
-				if (!each.getFileName().toString().toLowerCase().endsWith(JAR_PREFIX))
+				if (!each.getFileName().toString().toLowerCase().endsWith(JAR_FILE))
 					continue;
-				fileNames.add(each.toUri().toURL());
+				fileNames.add(each);
 			}
 		} catch (IOException e) {
 			LOG.error(e.getMessage(), e);
@@ -95,22 +100,40 @@ public class PluginServiceLoader implements PluginIntegrator {
 	
 	@Override
 	public ObservableMap<String, Plugin> loadPlugins(Path... paths) {
+		
+		ServiceLoader<Plugin> internal = ServiceLoader.load(Plugin.class);
+		Iterator<Plugin> internalIt = internal.iterator();
+		while (internalIt.hasNext()) {
+			Plugin plugin = internalIt.next();
+			if (plugins.get(plugin.id()) != null) {
+				// && CoreUtils.versionCompare(plugins.get(plugin.id()).version(), plugin.version()) <= 0) {
+				continue;
+			}
+			plugins.put(plugin.id(), plugin);
+			LOG.trace("Found and added internal plugin: {}", plugin.id());
+		}
+		
+		if (!plugins.isEmpty())
+			return plugins;
+		
 		for (Path path : paths) {
 			if (!Files.exists(path)) {
 				LOG.warn("Path {} doesn't exist.", path.toString());
 				continue;
 			}
-			Pair<ServiceLoader<Plugin>, ClassLoader> pluginServiceLoaderPair = loadIntegrations(path, Plugin.class);
-			ServiceLoader<Plugin> pluginServiceLoader = pluginServiceLoaderPair.getKey();
-			Iterator<Plugin> iterator = pluginServiceLoader.iterator();
-			while (iterator.hasNext()) {
-				Plugin plugin = iterator.next();
-				if (plugins.get(plugin.id()) != null) {
-					// && CoreUtils.versionCompare(plugins.get(plugin.id()).version(), plugin.version()) <= 0) {
-					continue;
+			List<Pair<ServiceLoader<Plugin>, ClassLoader>> integrations = loadIntegrations(path, Plugin.class);
+			for (Pair<ServiceLoader<Plugin>, ClassLoader> integration : integrations) {
+				ServiceLoader<Plugin> serviceLoader = integration.getKey();
+				Iterator<Plugin> iterator = serviceLoader.iterator();
+				while (iterator.hasNext()) {
+					Plugin plugin = iterator.next();
+					if (plugins.get(plugin.id()) != null) {
+						// && CoreUtils.versionCompare(plugins.get(plugin.id()).version(), plugin.version()) <= 0) {
+						continue;
+					}
+					plugins.put(plugin.id(), plugin);
+					LOG.trace("Found and added plugin: {}", plugin.id());
 				}
-				plugins.put(plugin.id(), plugin);
-				LOG.trace("Found and added plugin: {}", plugin.id());
 			}
 		}
 		return plugins;
