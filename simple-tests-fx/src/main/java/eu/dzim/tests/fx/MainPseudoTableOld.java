eb2d122fc725023e1a2252c295b223de1aaa6e19
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2017-10-04 15:22:37 +0200

    renamed current PseudoTable main file to make clear its obsolete

--- simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainPseudoTableOld.java
+++ simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainPseudoTableOld.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainPseudoTable extends Application {
+public class MainPseudoTableOld extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		
