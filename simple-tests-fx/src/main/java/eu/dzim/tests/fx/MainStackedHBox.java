c60f396550234a54375845a4f80b61a0e5dcb93e
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2017-10-04 15:14:25 +0200

    refactoring

--- simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainStackedHBox.java
+++ simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainStackedHBox.java
@@ -6,7 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class MainStack extends Application {
+public class MainStackedHBox extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		
