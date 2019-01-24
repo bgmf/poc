8b3104092111267dd5126641052d7885d3903d0b
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2019-01-24 17:30:04 +0100

    there was a strange request - updated it accordingly

--- simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainMultiWindow.java
+++ simple-tests-fx/src/main/java/eu/dzim/tests/fx/MainMultiWindow.java
@@ -5,9 +5,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.Window;
 
 public class MainMultiWindow extends Application {
     public static void main(String[] args) {
@@ -42,6 +44,7 @@ public class MainMultiWindow extends Application {
 
     private void showSecondaryStage() {
 
+        /*
         Stage stage = new Stage(StageStyle.DECORATED);
         // WINDOW_MODAL = only modal to its owner window(s)
         // APPLICATION_MODAL = modal to every window - Dialogs and alike
@@ -58,5 +61,46 @@ public class MainMultiWindow extends Application {
 
         stage.setScene(scene);
         stage.show();
+        */
+
+        DialogStage dialog = new DialogStage(StageStyle.DECORATED, Modality.APPLICATION_MODAL, primaryStage, -1, -1, 320, 240);
+        // dialog.show();
+        dialog.showAndWait();
+    }
+}
+
+class DialogStage extends Stage {
+
+    private final double width;
+    private final double height;
+
+    public DialogStage(StageStyle style, Modality modality, Window owner, double minWidth, double minHeight, double width, double height) {
+
+        super(style);
+
+        initModality(modality);
+        initOwner(owner);
+
+        if (minWidth > 0)
+            setMinWidth(minWidth);
+        if (minHeight > 0)
+            setMinHeight(minHeight);
+
+        this.width = width;
+        this.height = height;
+
+        createContent();
+    }
+
+    private void createContent() {
+        Pane content = createContentPane();
+        Scene scene = new Scene(content, width, height);
+        setScene(scene);
+    }
+
+    private Pane createContentPane() {
+        BorderPane root = new BorderPane();
+        root.setCenter(new Label("Secondary Window Content"));
+        return root;
     }
 }
