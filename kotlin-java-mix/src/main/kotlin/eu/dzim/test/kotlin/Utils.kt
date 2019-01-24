d4de629074ec5b1d4321317421b9dab7a29ce484
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2019-01-24 16:30:17 +0100

    add utils with Guice config, extension method for loggers (@Any class)
    + translation init

---
+++ kotlin-java-mix/src/main/kotlin/eu/dzim/test/kotlin/Utils.kt
@@ -0,0 +1,29 @@
+package eu.dzim.test.kotlin
+
+import com.google.inject.AbstractModule
+import eu.dzim.shared.resource.BaseResource
+import eu.dzim.shared.resource.Resource
+import eu.dzim.test.model.TestModel
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.util.*
+
+class ResourceImpl : BaseResource("i18n", "strings", Locale.ENGLISH)
+
+// could use eu.dzim.shared.guice.BaseGuiceModule instead
+class GuiceModule(private val testModel: TestModel) : AbstractModule() {
+
+    private val resource = ResourceImpl()
+
+    override fun configure() {
+        logger.info("Configure Guice.")
+        bind(Resource::class.java).toInstance(resource)
+        bind(TestModel::class.java).toInstance(testModel)
+    }
+
+    companion object {
+        private val logger by lazy { getLogger() }
+    }
+}
+
+inline fun Any.getLogger(): Logger = LogManager.getLogger(this::class.qualifiedName)
