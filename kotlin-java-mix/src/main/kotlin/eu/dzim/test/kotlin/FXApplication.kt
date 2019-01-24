328bf22cc9d8505e6602fa9fb892a586a07562c3
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2019-01-24 16:33:49 +0100

    init Guice, init TornadoFX DI, integrate TornadoFX-View into the app
    -> here, we use the views #getView function, since we are not a TornadoFX-App

--- kotlin-java-mix/src/main/kotlin/eu/dzim/test/kotlin/FXApplication.kt
+++ kotlin-java-mix/src/main/kotlin/eu/dzim/test/kotlin/FXApplication.kt
@@ -1,10 +1,13 @@
 package eu.dzim.test.kotlin
 
+import com.google.inject.Guice
 import eu.dzim.test.model.TestModel
 import javafx.application.Application
 import javafx.scene.Scene
-import javafx.scene.layout.Pane
+import javafx.scene.layout.BorderPane
 import javafx.stage.Stage
+import tornadofx.*
+import kotlin.reflect.KClass
 
 class FXApplication : Application() {
 
@@ -12,9 +15,15 @@ class FXApplication : Application() {
 
         val model = TestModel().apply {
             testString = "test"
-        }.run {
-            println(testString)
-            println(testStringProperty().get())
+        }.also {
+            println(it.testString)
+            println(it.testStringProperty().get())
+        }
+
+        Guice.createInjector(INJECTION_STAGE, GuiceModule(model)).apply {
+            FX.dicontainer = object : DIContainer {
+                override fun <T : Any> getInstance(type: KClass<T>): T = getInstance(type.java)
+            }
         }
 
         primaryStage?.apply {
@@ -22,10 +31,11 @@ class FXApplication : Application() {
             minHeight = 480.0
             title = "FX"
 
-            scene = Pane().let {
+            scene = BorderPane().let {
                 // init pane
+                it.center = TornadoView().getView()
                 // transform it into a scene
-                Scene(it) // .apply { more stuff }
+                Scene(it, minWidth, minHeight) // .apply { more stuff }
             }
         }?.show()
     }
