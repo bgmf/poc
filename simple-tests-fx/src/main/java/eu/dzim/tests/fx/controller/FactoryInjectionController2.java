85f726c95f5e36cfada8164da9f9afed46a7a8cf
Daniel Zimmermann <daniel.zimmermann@cnlab.ch>
2019-05-20 12:48:33 +0200

    fix expression binding to controller-local properties

--- simple-tests-fx/src/main/java/eu/dzim/tests/fx/controller/FactoryInjectionController2.java
+++ simple-tests-fx/src/main/java/eu/dzim/tests/fx/controller/FactoryInjectionController2.java
@@ -2,6 +2,10 @@ package eu.dzim.tests.fx.controller;
 
 import eu.dzim.tests.fx.MainWithControllerFactory2.Model;
 import eu.dzim.tests.fx.MainWithControllerFactory2.ModelInjector;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,12 +14,35 @@ public class FactoryInjectionController2 implements ModelInjector {
 
     @FXML private Button increaseButton;
 
-    @FXML private Model model;
+    private Model model;
+
+    /*
+     * to bind to sth within the controller, you need properties
+     * and you need all accessor methods - for whatever reason
+     */
+    private StringProperty textBinder = new SimpleStringProperty(this, "textBinder", "TEST");
+    public String getTextBinder() { return textBinder.get(); }
+    public void setTextBinder(String value) { textBinder.set(value); }
+    public StringProperty textBinderProperty() { return textBinder; }
+
+    // if you have something like the model, not wrapped into a ObjectProperty or so,
+    // create additional property accessor methods for the required properties
+    public String getText() { return model.textProperty().get(); }
+    public void setText(String value) { model.setText(value); }
+    public StringProperty textProperty() { return model.textProperty(); }
+
+    // wrap your model, if you want direct access
+    private ObjectProperty<Model> modelWrapper = new SimpleObjectProperty<>(this, "modelWrapper", null);
+    public Model getModelWrapper() { return modelWrapper.get(); }
+    public void setModelWrapper(Model modelWrapper) { this.modelWrapper.set(modelWrapper); }
+    public ObjectProperty<Model> modelObjectProperty() { return modelWrapper; }
 
     @Override
     public void setModel(Model model) {
         System.out.println(getClass().getName() + " is receiving model.");
         this.model = model;
+        textBinder.bind(model.textProperty());
+        setModelWrapper(model);
     }
 
     @FXML
